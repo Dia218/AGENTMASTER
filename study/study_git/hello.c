89a97aa6475b7c4eb7845e72e620bf90a9f15853
dia218 <sang218@kyonggi.ac.kr>
2023-04-25 00:10:24 +0900

    0425 이상은 영어버전으로 수정

--- study/study_git/hello.c
+++ study/study_git/hello.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0451e1516c6343a96e3c64e838566056f98794f05e7c42fd581ff0903d55f095
-size 323
+oid sha256:1fa6af1e72e638a5c9dfb26f42b48a836014c79e063dba88c54540835cdf1ed4
+size 286
