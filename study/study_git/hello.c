d704e724e86bd7234213b68f6f67ef7aeaa2d4db
Wheecheol Jeong <stup1db4nana@gmail.com>
2023-05-24 19:39:52 +0900

    staged test files
    duh

--- study/study_git/hello.c
+++ study/study_git/hello.c
@@ -2,20 +2,9 @@
 
 int main() 
 {
-	printf("Hello!!\n");
-	printf("***************************************\n");
+	printf("my back hurts!!\n");
+	printf("le wholesome year of the linux desktop!!!1!!\n");
 
-	printf("(안녕하세요. 상상기업 AGENTMASTER팀에서 데이터베이스 파트를 맡은 팀장 이동헌입니다.)\n");
-	printf("(이번 프로젝트를 성공적으로 마무리 짓기 위해 으쌰으쌰 힘냅시다~ 화이팅!)\n");
-	printf("(앞으로도 잘해보아요~)\n");
-
-	printf("꺼어억\n");
-	printf("배부르다\n");
-	printf("'stup1db4nana' is not in the sudoers file. This incident will be reported.\n");
-	printf("(Please feel free to edit this part)\n");
-	printf("(You can add some codes)\n");
-	printf("(However, creating another file and adding it is invalid)\n");
-	printf("(bruhh)\n");
 
 	return 0;
 }
