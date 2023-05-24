938ba1fa2b21277b192ead4eddbdd285d1adcafd
Wheecheol Jeong <stup1db4nana@gmail.com>
2023-05-24 19:41:40 +0900

    bruhh

--- study/study_git/hello.c
+++ study/study_git/hello.c
@@ -3,8 +3,8 @@
 int main() 
 {
 	printf("my back hurts!!\n");
-	printf("le wholesome year of the linux desktop!!!1!!\n");
-
+	//printf("le wholesome year of the linux desktop!!!1!!\n");
+	printf("all heil windows! all heil microsoft!");
 
 	return 0;
 }
