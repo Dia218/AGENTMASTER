11e03bd5fe054f3fc27625b27520adb8e9b1d246
Wheecheol Jeong <stup1db4nana@gmail.com>
2023-05-24 19:41:40 +0900

    Revert "bruhh"
    This reverts commit 938ba1fa2b21277b192ead4eddbdd285d1adcafd.

--- study/study_git/hello.c
+++ study/study_git/hello.c
@@ -3,8 +3,8 @@
 int main() 
 {
 	printf("my back hurts!!\n");
-	//printf("le wholesome year of the linux desktop!!!1!!\n");
-	printf("all heil windows! all heil microsoft!");
+	printf("le wholesome year of the linux desktop!!!1!!\n");
+
 
 	return 0;
 }
