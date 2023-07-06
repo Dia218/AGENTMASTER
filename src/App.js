f1e4ce37c323e1681c634e6358c56772d70e6a38
jeongeunhq <dmsgk991@naver.com>
2023-07-06 16:30:08 +0900

    정은하_차트 새페이지

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
-
+import Rechart2 from "./Rechart_2";
+import Rechart1 from "./Rechart_1";
+import Search from "./search";
+import ArticleList from './article';
+import Table from './Table';
 function App() {
   return (
-    <div className="App">
+    
+    <div >
+  <div style={{ display: "flex" }}>
+    <div style={{ width: 600, height: 400 }}>
+      <Rechart1 />
+      <Table/>
+      <Search />
     </div>
+    <div style={{ width: 600, height: 400 }}>
+      <Rechart2 />
+    </div>
+   
+  </div>
+  <div>
+        <ArticleList />
+      </div>
+</div>
+
   );
 }
 
