c1dce3bd5477f3da25046a6a45d2286bac1b91f1
jeongeunhq <dmsgk991@naver.com>
2023-07-09 15:55:29 +0900

    정은하 차트새페이지 수정 인터페이스 구현 완료

--- AgentMaster_frontend/src/article.js
+++ AgentMaster_frontend/src/article.js
@@ -1,33 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
 function ArticleList() {
-    const [articles, setArticles] = useState([]);
-  
-    useEffect(() => {
-      fetchArticles();
-    }, []);
-  
-    const fetchArticles = async () => {
-      try {
-        const response = await fetch('/api/articles'); 
-        const data = await response.json();
-        setArticles(data);
-      } catch (error) {
-        console.error('Error fetching articles:', error);
-      }
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const socket = socketIOClient('/'); // 소켓 연결
+
+    // 서버로부터 업데이트된 데이터 수신
+    socket.on('articles', (data) => {
+      setArticles(data);
+    });
+
+    // 임시 데이터
+    const temporaryData = [
+      { id: 1, title: 'Article 1' },
+      { id: 2, title: 'Article 2' },
+      { id: 3, title: 'Article 3' },
+    ];
+    setArticles(temporaryData);
+
+    return () => {
+      socket.disconnect(); // 컴포넌트가 언마운트될 때 소켓 연결 해제
     };
-  
-    return (
-      <div className='Article'>
-        <ul>
-          {articles.map((article) => (
-            <li key={article.id}>{article.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-  
-  export default ArticleList;
+  }, []);
+
+  return (
+    <div className='Article'>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ArticleList;
 
 
 
