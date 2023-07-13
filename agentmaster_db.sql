64e9d78e1d8cda85db1a5d8c5d753bbe60cea949
stup1db4nana <128582718+stup1db4nana@users.noreply.github.com>
2023-07-13 15:39:30 +0900

    Update agentmaster_db.sql
    merged sql lines

--- agentmaster_db.sql
+++ agentmaster_db.sql
@@ -1,17 +1,27 @@
+/*스키마 생성*/
+
+CREATE SCHEMA IF NOT EXISTS "AGENTMASTER"
+    AUTHORIZATION postgres;
+
 /*고객*/
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Customer"
 (
-    customer_id varchar(12) NOT NULL,
-    password    varchar(12) NOT NULL,
-    e_mail      text        NOT NULL,
-    simul_money integer     NOT NULL,
-    CONSTRAINT Customer_pkey PRIMARY KEY (customer_id)
+    customer_id  varchar(20)     NOT NULL,
+    password     varchar(20)     NOT NULL,
+    e_mail       text            NOT NULL,
+    simul_money  integer         NOT NULL,
+    total_return integer         NOT NULL,
+    total_range  numeric(10, 2)   NOT NULL,
+    CONSTRAINT Customer_pkey PRIMARY KEY (customer_id),
+	CONSTRAINT customer_id_between CHECK(customer_id NOT BETWEEN 'ㄱ' AND '힣'),
+	CONSTRAINT customer_email_check CHECK (e_mail LIKE '%@%.%')
 )
     TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS "AGENTMASTER"."Customer"
     OWNER to postgres;
+
 
 /*
 =======================
@@ -21,7 +31,7 @@ ALTER TABLE IF EXISTS "AGENTMASTER"."Customer"
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Field"
 (
-    field_name varchar(20) not null,
+    field_name varchar(100) not null,
 
     constraint "Field_pkey" primary key (field_name)
 )
@@ -35,10 +45,12 @@ ALTER TABLE IF EXISTS "AGENTMASTER"."Field"
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Stock"
 (
-    stock_id   integer     NOT NULL,
-    stock_name varchar(20) NOT NULL,
-    field_name varchar(20) NOT NULL,
+    stock_id   varchar(20)  NOT NULL,
+    stock_name varchar(100) NOT NULL,
+    field_name varchar(100) NOT NULL,
     CONSTRAINT Stock_pkey PRIMARY KEY (stock_id),
+    CONSTRAINT stock_name_unique UNIQUE (stock_name),
+	CONSTRAINT stock_id_check CHECK (stock_id LIKE '______'),
     CONSTRAINT field_name_fkey FOREIGN KEY (field_name)
         REFERENCES "AGENTMASTER"."Field" (field_name) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -55,11 +67,11 @@ ALTER TABLE IF EXISTS "AGENTMASTER"."Stock"
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Stock_info"
 (
-    stock_id          integer       NOT NULL,
+    stock_id          varchar(20)   NOT NULL,
     stock_date        date          NOT NULL,
     stock_price       integer       NOT NULL,
-    diff_from_prevday numeric(3, 1) NOT NULL,
-    range             numeric(3, 1) NOT NULL,
+    diff_from_prevday integer       NOT NULL,
+    range             numeric(4, 2) NOT NULL,
     CONSTRAINT Stock_info_pkey PRIMARY KEY (stock_id, stock_date),
     CONSTRAINT stock_id_fk FOREIGN KEY (stock_id)
         REFERENCES "AGENTMASTER"."Stock" (stock_id) MATCH SIMPLE
@@ -77,10 +89,11 @@ ALTER TABLE IF EXISTS "AGENTMASTER"."Stock_info"
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Simulation"
 (
-    customer_id    varchar(12) NOT NULL,
-    stock_id       integer     NOT NULL,
-    rate_return    integer     NOT NULL,
-    stock_holdings integer     NOT NULL,
+    customer_id    varchar(20)   NOT NULL,
+    stock_id       varchar(20)   NOT NULL,
+    simul_return   integer       NOT NULL,
+    simul_range    NUMERIC(10, 2) NOT NULL,
+    simul_holdings integer       NOT NULL,
     CONSTRAINT Simulation_pkey PRIMARY KEY (customer_id, stock_id),
     CONSTRAINT customer_id_fk FOREIGN KEY (customer_id)
         REFERENCES "AGENTMASTER"."Customer" (customer_id) MATCH SIMPLE
@@ -102,8 +115,8 @@ ALTER TABLE IF EXISTS "AGENTMASTER"."Simulation"
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Bookmark"
 (
-    customer_id varchar(12) NOT NULL,
-    stock_id    integer     NOT NULL,
+    customer_id varchar(20) NOT NULL,
+    stock_id    varchar(20) NOT NULL,
     CONSTRAINT Bookmark_pkey PRIMARY KEY (customer_id, stock_id),
     CONSTRAINT customer_id_fk FOREIGN KEY (customer_id)
         REFERENCES "AGENTMASTER"."Customer" (customer_id) MATCH SIMPLE
@@ -150,7 +163,7 @@ CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Article"
     body       text        not null,
     link       text        not null,
     group_name varchar(30),
-    field_name varchar(20) not null,
+    field_name varchar(100) not null,
 
     constraint "Article_pkey" primary key (article_id),
     constraint "Articles_group_name_fkey" foreign key (group_name)
@@ -171,7 +184,7 @@ alter table if exists "AGENTMASTER"."Article"
 /*스크랩*/
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Article_Scrap"
 (
-    customer_id varchar(12) not null,
+    customer_id varchar(20) not null,
     article_id  int         not null,
 
     constraint "Article_Scrap_pkey" primary key (customer_id, article_id),
