5b4da04b1ff368769111225fcd7a0852c88bd443
miminu <kios917@gmail.com>
2023-08-10 20:49:40 +0900

    프론트 이현수 모의투자 페이지 병합 완료

--- AgentMaster_frontend/src/App.js
+++ AgentMaster_frontend/src/App.js
@@ -8,7 +8,7 @@ import ChartMain from './router/ChartMain';
 import UserPage from './router/UserPage';
 import SimulMain from './router/SimulMain';
 import NotFound from './router/NotFound';
-import { useState } from 'react';
+import SimTrade from "./router/SimTrade";
 
 function App() {
   return (
@@ -21,6 +21,7 @@ function App() {
         <Route path='/chartDetail' element={<ChartDetail />} />
         <Route path='/userPage' element={<UserPage />} />
         <Route path='/simulMain' element={<SimulMain />} />
+        <Route path='/simulTrade' element={<SimTrade />} />
         <Route path='/*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
