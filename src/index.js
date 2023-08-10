208839b23f1c8f36ad96c817f65db62d25350cee
stup1db4nana <128582718+stup1db4nana@users.noreply.github.com>
2023-08-10 19:30:37 +0900

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppSimTradeScene from './AppSimTradeScene';
-import './App.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <AppSimTradeScene/>
-);
