4e1665089a9c016fa93e11422065e479ae37e3af
miminu <kios917@gmail.com>
2023-08-10 20:53:25 +0900

    프론트 이현수 모의투자 병합 완료

--- AgentMaster_frontend/src/component/Simul/Main_body.js
+++ AgentMaster_frontend/src/component/Simul/Main_body.js
@@ -2,11 +2,12 @@
 
 import "./css/Main_body.css";
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Main_body() {
     
     const [bodyList, setBodyList] = useState([]);   //api로 불러온 json 형식 데이터를 저장하는 배열
+    const navigate = useNavigate();
     const getBodyList = async () => {
         const json = await (    //api에 데이터 불러오는 코드
             await fetch("https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=3lxB%2F1OXwuo7FgMdLkt0cG6kMEVKc1fHITjq6%2F5aAF5wYK5UQbaKvl7JxcXkdZnLL6ao2N2U6NbUpMKXRA8NqQ%3D%3D&resultType=json&beginFltRt=20")
@@ -20,6 +21,7 @@ function Main_body() {
 
     const onClick = (e) => {        //클릭 리스너
         //console.log(e.currentTarget.id + "번 모의투자 화면으로 이동.");
+        navigate(`/simulTrade`);
     }
 
     const addComma = (num) => {     //현재가, 전일비 값을 불러올 때 자동으로 3자리 마다 콤마(,)를 붙이는 코드
