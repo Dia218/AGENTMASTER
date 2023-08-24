4c567d4612933e6b3e48b8d1dc64ecbce0cd8e8b
honey720 <101120544+honey720@users.noreply.github.com>
2023-08-24 21:47:42 +0900

    0824 update

--- agentmaster_db.sql
+++ agentmaster_db.sql
@@ -5,9 +5,10 @@ CREATE SCHEMA IF NOT EXISTS "AGENTMASTER"
 
 /*고객*/
 
-CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Customer"
+CREATE TABLE IF NOT EXISTS "AGENTMASTER"."User"
 (
-    customer_id     varchar(15)    NOT NULL,
+    user_id         BIGSERIAL      NOT NULL,
+    user_name       varchar(15)    NOT NULL,
     password        varchar(20)    NOT NULL,
     e_mail          text           NOT NULL,
     total_money     integer        NOT NULL,
@@ -16,30 +17,27 @@ CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Customer"
     stock_money     integer        NOT NULL,
     total_return    integer        NOT NULL,
     rank_range      numeric(10, 2) NOT NULL,
-    CONSTRAINT Customer_pkey PRIMARY KEY (customer_id),
-    CONSTRAINT customer_id_check CHECK (customer_id ~ '^[A-Za-z0-9]{5,15}$' AND customer_id !~ '^[0-9]{5,15}$'),
-    CONSTRAINT customer_password_check CHECK (password ~ '^[A-Za-z0-9]{8,20}$' AND password !~ '^[0-9]{8,20}$' AND
-                                              password !~ '^[A-Za-z]{8,20}$'),
-    CONSTRAINT customer_email_check CHECK (e_mail ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
-)
+    simul_ranking   integer        NOT NULL,
+    CONSTRAINT User_pkey PRIMARY KEY (user_id),
+    CONSTRAINT User_unique UNIQUE (user_name),
+    CONSTRAINT user_name_check CHECK (user_name ~ '^[A-Za-z0-9]{5,15}$' AND user_name !~ '^[0-9]{5,15}$'),
+    CONSTRAINT user_password_check CHECK (password ~ '^[A-Za-z0-9]{8,20}$' AND password !~ '^[0-9]{8,20}$' AND password !~ '^[A-Za-z]{8,20}$'),
+    CONSTRAINT user_email_check CHECK (e_mail ~ '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
+)   
     TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS "AGENTMASTER"."Customer"
+ALTER TABLE IF EXISTS "AGENTMASTER"."User"
     OWNER to postgres;
 
-
-/*
-=======================
-분야 merged from newsDB
-=======================
-*/
+/*분야*/
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Field"
 (
-    field_id   bigserial           not null,
-    field_name varchar(100) unique not null,
+    field_id   BIGSERIAL    NOT NULL,
+    field_name varchar(100) not null,
 
-    constraint "Field_id_pkey" primary key (field_id)
+    constraint Field_pkey primary key (field_id),
+    CONSTRAINT Field_unique UNIQUE (field_name)
 )
     TABLESPACE pg_default;
 
@@ -48,17 +46,18 @@ ALTER TABLE IF EXISTS "AGENTMASTER"."Field"
 
 /*주식*/
 
-
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Stock"
 (
-    stock_id   varchar(20)  NOT NULL,
+    stock_id   BIGSERIAL    NOT NULL,
+    stock_code varchar(20)  NOT NULL,
     stock_name varchar(100) NOT NULL,
-    field_name varchar(100) NOT NULL,
+    field_id   BIGINT       NOT NULL,
     CONSTRAINT Stock_pkey PRIMARY KEY (stock_id),
+    CONSTRAINT stock_code_unique UNIQUE (stock_code),
     CONSTRAINT stock_name_unique UNIQUE (stock_name),
-    CONSTRAINT stock_id_check CHECK (stock_id ~ '\d{6}'),
-    CONSTRAINT field_name_fkey FOREIGN KEY (field_name)
-        REFERENCES "AGENTMASTER"."Field" (field_name) MATCH SIMPLE
+    CONSTRAINT stock_id_check CHECK (stock_code ~ '\d{6}'),
+    CONSTRAINT field_id_fkey FOREIGN KEY (field_id)
+        REFERENCES "AGENTMASTER"."Field" (field_id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE RESTRICT
 )
@@ -73,7 +72,8 @@ ALTER TABLE IF EXISTS "AGENTMASTER"."Stock"
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Stock_info"
 (
-    stock_id           varchar(20)   NOT NULL,
+    stock_info_id      BIGSERIAL     NOT NULL,
+    stock_id           BIGINT        NOT NULL,
     stock_date         date          NOT NULL,
     stock_price        integer       NOT NULL,
     diff_from_prevday  integer       NOT NULL,
@@ -81,9 +81,10 @@ CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Stock_info"
     start_price        integer       NOT NULL,
     high_price         integer       NOT NULL,
     low_price          integer       NOT NULL,
-    trading_volume     integer       NOT NULL,
-    transaction_volume integer       NOT NULL,
-    CONSTRAINT Stock_info_pkey PRIMARY KEY (stock_id, stock_date),
+    trading_volume     bigint        NOT NULL,
+    transaction_volume bigint        NOT NULL,
+    CONSTRAINT Stock_info_pkey PRIMARY KEY (stock_info_id),
+    CONSTRAINT Stock_info_unique UNIQUE (stock_id, stock_date),
     CONSTRAINT stock_id_fk FOREIGN KEY (stock_id)
         REFERENCES "AGENTMASTER"."Stock" (stock_id) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -100,16 +101,18 @@ ALTER TABLE IF EXISTS "AGENTMASTER"."Stock_info"
 
 CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Simulation"
 (
-    customer_id     varchar(15)    NOT NULL,
-    stock_id        varchar(20)    NOT NULL,
+    simulation_id   BIGSERIAL      NOT NULL,
+    user_id         BIGINT         NOT NULL,
+    stock_id        BIGINT         NOT NULL,
     simul_return    integer        NOT NULL,
     simul_range     NUMERIC(10, 2) NOT NULL,
     simul_holdings  integer        NOT NULL,
     perchase_amount integer        NOT NULL,
     average_price   integer        NOT NULL,
-    CONSTRAINT Simulation_pkey PRIMARY KEY (customer_id, stock_id),
-    CONSTRAINT customer_id_fk FOREIGN KEY (customer_id)
-        REFERENCES "AGENTMASTER"."Customer" (customer_id) MATCH SIMPLE
+    CONSTRAINT Simulation_pkey PRIMARY KEY (simulation_id),
+    CONSTRAINT Simulation_unique UNIQUE (user_id, stock_id),
+    CONSTRAINT user_id_fk FOREIGN KEY (user_id)
+        REFERENCES "AGENTMASTER"."User" (user_id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     CONSTRAINT stock_id FOREIGN KEY (stock_id)
@@ -122,28 +125,6 @@ CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Simulation"
 ALTER TABLE IF EXISTS "AGENTMASTER"."Simulation"
     OWNER to postgres;
 
-
-/*북마크*/
-
-
-CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Bookmark"
-(
-    customer_id varchar(15) NOT NULL,
-    stock_id    varchar(20) NOT NULL,
-    CONSTRAINT Bookmark_pkey PRIMARY KEY (customer_id, stock_id),
-    CONSTRAINT customer_id_fk FOREIGN KEY (customer_id)
-        REFERENCES "AGENTMASTER"."Customer" (customer_id) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-    CONSTRAINT stock_id_fk FOREIGN KEY (stock_id)
-        REFERENCES "AGENTMASTER"."Stock" (stock_id) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-)
-    TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS "AGENTMASTER"."Bookmark"
-    OWNER to postgres;
 
 /*
 -----------------
@@ -211,25 +192,25 @@ alter table if exists "AGENTMASTER"."Article"
     OWNER to postgres;
 
 /*스크랩*/
-CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Article_Scrap"
+CREATE TABLE IF NOT EXISTS "AGENTMASTER"."Article_scrap"
 (
     scrap_id        bigserial     not null,
     user_id         bigint unique not null,
     article_link_id bigint unique not null,
 
     constraint "Scrap_id_pkey" primary key (scrap_id),
-    constraint "Article_Scrap_customer_id_fkey" foreign key (user_id)
+    constraint "Article_scrap_customer_id_fkey" foreign key (user_id)
         references "AGENTMASTER"."User" (user_id) match simple
         on update cascade
         on delete cascade,
-    constraint "Article_Scrap_news_id_fkey" foreign key (article_link_id)
+    constraint "Article_scrap_news_id_fkey" foreign key (article_link_id)
         references "AGENTMASTER"."Article_link" (article_id) match simple
         on update cascade
         on delete cascade
 )
     tablespace pg_default;
 
-alter table if exists "AGENTMASTER"."Article_Scrap"
+alter table if exists "AGENTMASTER"."Article_scrap"
     OWNER to postgres;
 
 /*기사 요약문*/
