f7a4139a700f59a69725771fa428dd79699dff00
programofktw <thkim000317@naver.com>
2023-09-05 22:25:55 +0900

    2023/09/05 ArticleService 제작

--- newstock/src/main/java/agentmaster/newstock/service/ArticleService.java
+++ newstock/src/main/java/agentmaster/newstock/service/ArticleService.java
@@ -1,5 +1,6 @@
 package agentmaster.newstock.service;
 
+import agentmaster.newstock.dto.articlePage.searchPage.SearchArticle;
 import agentmaster.newstock.repository.ArticleFileRepository;
 import agentmaster.newstock.repository.ArticleRepository;
 import agentmaster.newstock.dto.articlePage.mainPage.PreviewArticle;
@@ -12,4 +13,7 @@ public interface ArticleService {
 
     //뉴스 메인 페이지 뉴스 프리뷰 전달부
     public List<PreviewArticle> provideArticlesForPreview();
+
+    //뉴스 검색 페이지 검색 키워드에 따른 결과 전달부
+    public List<SearchArticle> provideArticleBySearch(String search);
 }
