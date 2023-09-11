efee4db8010afd288623bf4494e3406114e9dead
StandGive <SeoCoding123@gmail.com>
2023-09-12 01:07:00 +0900

    이서준 / REFACTOR: 두번째 뉴스 순서 파일 시스템 구조 재수정

--- news_flow.py
+++ news_flow.py
@@ -2,6 +2,7 @@ import openai
 
 openai.api_key = ""
 
+
 def llm_request(content):
     print("=== DATA UPDATE ===")
     """
@@ -11,49 +12,55 @@ def llm_request(content):
                        뉴스 요약을 거친 후 요약된 결과를 토대로 뉴스 순서를 정리합니다.
     """
     messages = []
-    messages.append({"role": "user", "content": content + "사건 흐름 중 뉴스 순서좀 년도에 맞게 총 3개로 나눠서 알려줘."})
-
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
+    messages.append(
+        {"role": "user", "content": content + "사건 흐름 중 뉴스 순서좀 년도에 맞게 총 3개로 나눠서 알려줘."}
     )
+
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
 
     chat_response = completion.choices[0].message.content
     messages.append({"role": "assistant", "content": chat_response})
 
     # 분리된 뉴스 정리
-    news_parts = chat_response.split('\n\n')
+    news_parts = chat_response.split("\n\n")
 
     news_dict = {
-        '1번': {'body': news_parts[0]},
-        '2번': {'body': news_parts[1]},
-        '3번': {'body': news_parts[2]}
+        "1번": {"body": news_parts[0]},
+        "2번": {"body": news_parts[1]},
+        "3번": {"body": news_parts[2]},
     }
 
     # 소개 부분 삭제 및 내용이 없는 경우 제거
     for key, value in news_dict.items():
-        value['body'] = '\n'.join([line for line in value['body'].split('\n')[1:] if line.strip()])
+        value["body"] = "\n".join(
+            [line for line in value["body"].split("\n")[1:] if line.strip()]
+        )
 
     formatted_results = []
 
     # 결과물 포맷 만들기
     current_number = 1  # 카운트 변수 초기화
     for key, value in news_dict.items():
-        if value['body']:  # 내용이 있는 경우에만 포맷 만들기
-            formatted_results.append({'role': 'user', 'content': f'{current_number}번: '})
-            formatted_results.append({'input_news': content, 'body': value['body']})
+        if value["body"]:  # 내용이 있는 경우에만 포맷 만들기
+            formatted_results.append(
+                {"role": "user", "content": f"{current_number}번: "}
+            )
+            formatted_results.append({"input_news": content, "body": value["body"]})
             current_number += 1  # 카운트 변수 증가
 
     print("=== FORMATTED RESULTS ===")
     for result in formatted_results:
         print(result)
 
-    # 딕셔너리를 텍스트 파일에 저장
-    with open("/Users/seojun/vscode/agentmaster/data/news_flow.txt", "w") as file:
+    # 딕셔너리를 텍스트 파일에 저장 (파일명 동적으로 변경)
+    with open(
+        f"/Users/seojun/vscode/agentmaster/data/issue_{content}.txt", "w"
+    ) as file:
         for result in formatted_results:
             file.write(str(result) + "\n")
 
     print("=== DATA SUCCESSFUL ===")
 
-keyword = '코로나19'
+
+keyword = "코로나19"
 llm_request(keyword)
