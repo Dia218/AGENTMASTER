f98e76e4f978f1823b7207bd0d536eda60e59e3d
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2023-11-02 15:28:34 +0900

    정은하 11/02 거래페이지 그래프 수정

--- AgentMaster_frontend/src/component/SimTrade/StockGraphChart.js
+++ AgentMaster_frontend/src/component/SimTrade/StockGraphChart.js
@@ -1,39 +1,93 @@
 import './css/StockGraphChart.css';
-import {
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    ResponsiveContainer, ReferenceLine,
-} from "recharts";
-import React from "react";
+import axios from 'axios';
+import React, { useState, useRef,useEffect} from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
+import ReactApexChart from 'react-apexcharts';
 
-export default function StockGraphChart({GraphChartInput}) {
-    return (
-        <ResponsiveContainer width="100%" height={400}>
-            <BarChart
-                data={GraphChartInput}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3"/>
-                <XAxis dataKey="name"/>
-                <YAxis/>
-                <Tooltip
-                    wrapperStyle={{color: "white"}}
-                />
-                <Legend/>
-                <ReferenceLine y={0} stroke="#000"/>
-                <Bar dataKey="pv" fill="blue"/>
-                <Bar dataKey="uv" fill="red"/>
-            </BarChart>
-        </ResponsiveContainer>
-    );
+export default function StockGraphChart({ GraphChartInput }) {
+    const [stockData, setStockData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+const getChart1 = async () => {
+  try{
+    const queryParams = new URLSearchParams(window.location.search);
+    const keywordFromURL = queryParams.get('keyword');
+    const getChart1Rep= await axios.get('http://localhost:8080/ChartData?stockId=${keywordFromURL}');
+    setStockData(getChart1Rep.data);
+    setLoading(false);
+  }catch(error) {
+    setLoading(false);
+  }
+}
+useEffect(() => {
+ getChart1();
+}, []);
+
+  const seriesData = [{
+    name: "종목가",
+    data: stockData.map(item => ({
+      x: new Date(item.stockDate).getTime(),
+      y: item.stockPrice,
+      stockDiff: item.stockDiff,
+      stockRange: item.stockRange
+    }))
+  }];
+
+  const options = {
+    chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    xaxis: {
+      type: 'datetime',
+    },
+    yaxis: {
+      title: {
+        text: '가격'
+      },
+    },
+    tooltip: {
+      x: {
+        format: 'dd MMM yyyy'
+      },
+      y: {
+        title: {
+          formatter: (val) => val 
+        }
+      },
+      custom: ({ series, seriesIndex, dataPointIndex, w }) => {
+        const item = stockData[dataPointIndex];
+        if (item && item.stockDiff !== undefined && item.stockRange !== undefined) {
+          return (
+            `<div class="custom-tooltip">
+              <span>종목가: ${item.stockPrice}</span><br>
+              <span>전일대비: ${item.stockDiff}</span><br>
+              <span>등락률: ${item.stockRange}%</span>
+            </div>`
+          );
+        }
+        return '';
+      }
+    }
+  };
+
+  return (
+    <div className='Rechart1'>
+      {loading ? (
+        <div className='loading_main'><LoadingOutlined />loading...</div>
+      ) : (
+        <ReactApexChart options={options} series={seriesData} type="line" height={350} width={700} />
+      )}
+    </div>
+  ); 
 }
