c64695bf7ee1e30ee2b6960c9dd45d5f65b48146
programofktw <thkim000317@naver.com>
2023-11-08 23:35:54 +0900

    2023/11/08 김태완 백엔드-프론트 연동 전체 확인

--- AgentMaster_frontend/src/component/SimTrade/StockGraphChart.js
+++ AgentMaster_frontend/src/component/SimTrade/StockGraphChart.js
@@ -92,7 +92,6 @@ const getChart1 = async () => {
     const keywordFromURL = queryParams.get('keyword');
     const getChart1Rep= await axios.get(`http://localhost:8080/ChartData?stockId=${keywordFromURL}`);
     setStockData(getChart1Rep.data.ChartData);
-
     setLoading(false);
   }catch(error) {
     setLoading(false);
@@ -168,6 +167,4 @@ useEffect(() => {
       )}
     </div>
   ); 
-
 }
-
